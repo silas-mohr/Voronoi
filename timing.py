f41fb31b8791e77f67594d95d0b5fc242e54fa7a
Silas M <smohr@princeton.edu>
2022-12-28 01:38:25 -0500

    Added better timing

--- timing.py
+++ timing.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import time
+from tqdm import tqdm
 
 from voronoi import Voronoi
 from utils import Point
@@ -8,22 +9,30 @@ from utils import Point
 
 def main():
     seed = 10
-    num_sites = [10, 20, 50, 100, 200, 500, 1000, 2500, 5000, 7500]
-    times = []
+    num_iterations = 15
+    num_sites = [10, 50, 100, 200, 500, 1000, 1500, 2000, 2500, 3000, 4000, 5000]
+
+    average_times = []
     np.random.seed(seed)
 
-    for num in num_sites:
-        points = np.random.random(size=(num, 2)) * 1000
-        sites = []
-        for site in points:
-            sites.append(Point(site[0], site[1]))
-        start = time.perf_counter()
-        v = Voronoi(sites)
-        v.compute()
-        total_time = time.perf_counter() - start
-        times.append(total_time)
-        print(f"Voronoi diagram of size {num} computed in {total_time:.3f} seconds")
-    plt.plot(num_sites, times, 'r')
+    for i in range(num_iterations):
+        times = []
+        for num in tqdm(num_sites):
+            points = np.random.random(size=(num, 2)) * 1000
+            sites = []
+            for site in points:
+                sites.append(Point(site[0], site[1]))
+            start = time.perf_counter()
+            v = Voronoi(sites)
+            v.compute()
+            total_time = time.perf_counter() - start
+            times.append(total_time)
+            # print(f"Voronoi diagram of size {num} computed in {total_time:.3f} seconds")
+        print(f"Finished round {i}")
+        average_times.append(times)
+    average_times = np.mean(average_times, axis=0)
+
+    plt.plot(num_sites, average_times, 'r')
 
     x = np.linspace(1, num_sites[-1], 100)
 
